33f86ea30f514ff4568c4e15eb1f5bb71d93ebcd
Daniel Saraiva <danieldsvn@gmail.com>
2022-06-22 16:47:56 -0300

    Cria inputs radio buttons para filtros e button para realizar a pesquisa

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,11 +7,44 @@ function SearchBar() {
   return (
     <div>
       {searchBar && (
-        <input
-          type="text"
-          data-testid="search-input"
-        />
-
+        <div>
+          <input
+            type="text"
+            data-testid="search-input"
+          />
+          <div>
+            <label htmlFor="ingredient-search-radio">
+              Ingredient
+              <input
+                id="ingredient-search-radio"
+                data-testid="ingredient-search-radio"
+                type="radio"
+              />
+            </label>
+            <label htmlFor="name-search-radio">
+              Name
+              <input
+                id="name-search-radio"
+                data-testid="name-search-radio"
+                type="radio"
+              />
+            </label>
+            <label htmlFor="first-letter-search-radio">
+              First Letter
+              <input
+                id="first-letter-search-radio"
+                data-testid="first-letter-search-radio"
+                type="radio"
+              />
+            </label>
+            <button
+              type="button"
+              data-testid="exec-search-btn"
+            >
+              Search
+            </button>
+          </div>
+        </div>
       )}
     </div>
   );
