697a2bed638db6515a93661650443433ee1dd0f3
Daniel Saraiva <danieldsvn@gmail.com>
2022-06-22 17:20:12 -0300

    Controla radio buttons no estado local

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,22 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import MyContext from '../context/context';
 
 function SearchBar() {
   const { searchBar } = useContext(MyContext);
+  const [radiosControl, setRadiosControl] = useState({
+    radioType: '',
+  });
+
+  const handleRadiosInputs = ({ target }) => {
+    console.log(target.value);
+    setRadiosControl({
+      radioType: target.value,
+    });
+  };
+
+  const handleSearchButton = () => {
+    console.log(radiosControl.radioType);
+  };
 
   return (
     <div>
@@ -16,30 +30,40 @@ function SearchBar() {
             <label htmlFor="ingredient-search-radio">
               Ingredient
               <input
+                name="search-radio"
                 id="ingredient-search-radio"
                 data-testid="ingredient-search-radio"
                 type="radio"
+                value="ingredient-search-radiot"
+                onClick={ handleRadiosInputs }
               />
             </label>
             <label htmlFor="name-search-radio">
               Name
               <input
+                name="search-radio"
                 id="name-search-radio"
                 data-testid="name-search-radio"
                 type="radio"
+                value="name-search-radio"
+                onClick={ handleRadiosInputs }
               />
             </label>
             <label htmlFor="first-letter-search-radio">
               First Letter
               <input
+                name="search-radio"
                 id="first-letter-search-radio"
                 data-testid="first-letter-search-radio"
                 type="radio"
+                value="first-letter-search-radio"
+                onClick={ handleRadiosInputs }
               />
             </label>
             <button
               type="button"
               data-testid="exec-search-btn"
+              onClick={ handleSearchButton }
             >
               Search
             </button>
