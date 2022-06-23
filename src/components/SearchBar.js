2979f1443438b3de162be5ba1ecad61cb127ebdc
Daniel Saraiva <danieldsvn@gmail.com>
2022-06-23 01:24:31 -0300

    implementa requisição de api para busca de drinks

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,5 @@
 import React, { useContext, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import MyContext from '../context/context';
 
 function SearchBar() {
@@ -23,8 +24,9 @@ function SearchBar() {
     });
   };
 
-  const handleSearchButton = async () => {
-    const { radioType, inputText } = searchBarData;
+  const location = useLocation();
+
+  const foodSearch = async (radioType, inputText) => {
     if (radioType === 'ingredient-search-radio') {
       const ingredientSearch = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${inputText}`);
       const data = await ingredientSearch.json();
@@ -52,6 +54,55 @@ function SearchBar() {
           searchResult: data,
         });
       }
+    }
+  };
+  const drinkSearch = async (radioType, inputText) => {
+    if (radioType === 'ingredient-search-radio') {
+      const ingredientEndPoint = 'www.thecocktaildb.com/api/json/v1/1/filter.php?i=';
+      const ingredientSearch = await fetch(`${ingredientEndPoint}${inputText}`);
+      const data = await ingredientSearch.json();
+      setSearchBarData({
+        ...searchBarData,
+        searchResult: data,
+      });
+    }
+    if (radioType === 'name-search-radio') {
+      const nameEndPoint = 'www.thecocktaildb.com/api/json/v1/1/search.php?i=';
+      const nameSearch = await fetch(`${nameEndPoint}${inputText}`);
+      const data = await nameSearch.json();
+      setSearchBarData({
+        ...searchBarData,
+        searchResult: data,
+      });
+    }
+    if (radioType === 'first-letter-search-radio') {
+      if (inputText.length > 1) {
+        global.alert('Your search must have only 1 (one) character');
+      } else {
+        const firstLetterEndPoint = 'www.thecocktaildb.com/api/json/v1/1/search.php?f=';
+        const firstLetterSearch = await fetch(`${firstLetterEndPoint}${inputText}`);
+        const data = await firstLetterSearch.json();
+        setSearchBarData({
+          ...searchBarData,
+          searchResult: data,
+        });
+      }
+    }
+  };
+
+  // const history = useHistory();
+  const handleSearchButton = () => {
+    const { pathname } = location;
+    const { radioType, inputText } = searchBarData;
+    if (pathname === '/foods') {
+      foodSearch(radioType, inputText);
+      // const { meals } = searchResult;
+      // if (meals.length === 0) history.push(`/foods/${meals[0].idMeal}`);
+    }
+    if (pathname === '/drinks') {
+      drinkSearch(radioType, inputText);
+      // const { drinks } = searchResult;
+      // if (drinks.length === 0) history.push(`/drinks/${drinks[0].idDrink}`);
     }
   };
 
