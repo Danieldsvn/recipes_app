37ccf2fc4e2a817bf1ca4068115011855dd9cf6e
Daniel Saraiva <danieldsvn@gmail.com>
2022-06-24 16:29:07 -0300

    Cria componente DetailsHeader.js

--- src/pages/FoodDetails.js
+++ src/pages/FoodDetails.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import RecommendedCard from '../components/RecommendedCard';
+import DetailsHeader from '../components/DetailsHeader';
 
 function FoodDetails() {
   return (
     <div>
-      <header className="details-foods-header">
-        <h1 data-testid="recipe-title">Nome da receita</h1>
-        <img data-testid="recipe-photo" src="src" alt="alt" />
-        <p data-testid="recipe-category">categoria</p>
-        <button data-testid="share-btn" type="button">compartilhar</button>
-        <button data-testid="favorite-btn" type="button">favorito</button>
-      </header>
+      <DetailsHeader />
       <section className="ingredients-section">
         <h2>Ingredients</h2>
         <ul className="ingredients-list">
