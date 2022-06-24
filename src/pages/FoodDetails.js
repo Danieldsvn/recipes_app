61200b419a44fd4f8052ed7ddd2cd51690d4044c
Daniel Saraiva <danieldsvn@gmail.com>
2022-06-24 15:22:33 -0300

    Iniciando tela de detalhes de foods

--- src/pages/FoodDetails.js
+++ src/pages/FoodDetails.js
@@ -2,7 +2,37 @@ import React from 'react';
 
 function FoodDetails() {
   return (
-    <div>FoodDetails</div>
+    <div>
+      <img src="src" alt="alt" />
+      <div>
+        <h1>Nome da receita</h1>
+        <p>categoria</p>
+        <button type="button">compartilhar</button>
+        <button type="button">favorito</button>
+      </div>
+      <div>
+        <h2>Ingredients</h2>
+        <ul>
+          <li>ingrediente 1</li>
+        </ul>
+      </div>
+      <div>
+        <h2>Instructions</h2>
+        <div>
+          <p>text...</p>
+        </div>
+      </div>
+      <div>
+        <h2>Video</h2>
+        <link />
+      </div>
+      <div>
+        <h2>Recomended</h2>
+        <div>
+          <p>Cards de bebidas recomendadas</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
