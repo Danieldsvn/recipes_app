aaec0264d2e84f10ada372c562e0b92ec9b50184
Daniel Saraiva <danieldsvn@gmail.com>
2022-06-28 19:42:33 -0300

    fix: retira lista de ingredientes 'null'

--- src/hooks/getIngredientsAndMesures.js
+++ src/hooks/getIngredientsAndMesures.js
@@ -2,10 +2,12 @@ const getIngredientsAndMeasures = (mealOrDrink) => {
   const mealOrDrinksEntries = Object.entries(mealOrDrink[0]);
   const strIngredientsEntries = mealOrDrinksEntries
     .filter((entry) => entry[0]
-      .includes('strIngredient') && entry[1] !== '');
+      .includes('strIngredient') && entry[1] !== '')
+    .filter((entry) => entry[1] !== null);
   const strMeasuresEntries = mealOrDrinksEntries
     .filter((entry) => entry[0]
-      .includes('strMeasure') && entry[1] !== ' ');
+      .includes('strMeasure') && entry[1] !== ' ')
+    .filter((entry) => entry[1] !== null);
   const ingredientsList = strIngredientsEntries.map((ingredient, index) => (
     `${ingredient[1]}: ${strMeasuresEntries[index][1]}`));
   return ingredientsList;
