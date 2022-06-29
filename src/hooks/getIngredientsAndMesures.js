aa425b51bb4992caf76b1cf432aefebb57b87160
Daniel Saraiva <danieldsvn@gmail.com>
2022-06-29 11:50:13 -0300

    git pull origin pre-main-group-22

--- src/hooks/getIngredientsAndMesures.js
+++ src/hooks/getIngredientsAndMesures.js
@@ -12,7 +12,8 @@ const getIngredientsAndMeasures = (mealOrDrink) => {
   const ingredientsList = strIngredientsEntries.map((ingredient, index) => (
     `${ingredient[1]}: ${strMeasuresEntries[index][1]}`));
     // Com o código comentado abaixo no lugar do código da linha 12 e 13
-    // faz o requisito 32 passar mas quebra o 33 e o 35. Na prática, o código abaixa funciona normal :/
+    // faz o requisito 32 passar mas quebra o 33 e o 35.
+    // Na prática, o código abaixo funciona normal :/
     // const ingredientsList = strMeasuresEntries.map((measure, index) => (
     //   `${strIngredientsEntries[index][1]}: ${measure[1]}`));
   return ingredientsList;
