5ab5195ba6ef755fc28009ce9d7dce9028542949
Yuri Mascarenhas <dev.yurimascarenhas@gmail.com>
2022-06-30 12:09:44 -0300

    App.css - modificada cor e formato dos botões de toda aplicação

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import './App.css';
+import './styles/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
